74c7e36f912a45ebfb4d9b4c9a96ec730340c392
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-04-08 13:52:20 -0300

    refatora login page usando shadcn

--- src/features/login/index.tsx
+++ src/features/login/index.tsx
@@ -4,6 +4,20 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import * as authService from "../../services/auth";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 const loginSchema = yup.object({
   email: yup.string().required().email(),
   password: yup.string().required().min(5),
@@ -14,108 +28,94 @@ type LoginType = yup.InferType<typeof loginSchema>;
 export default function Login() {
   const navigate = useNavigate();
 
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm({
+  const form = useForm({
     resolver: yupResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
   });
 
   const onSubmit = async ({ email, password }: LoginType) => {
     try {
-      if (!email || !password) {
-        setError("root", { message: "Informe o e-mail e a senha." });
-        return;
-      }
-
       await authService.doLogIn(email, password);
 
       if (authService.isLoggedIn() !== true) {
-        setError("root", { message: "E-mail ou senha incorretos." });
+        form.setError("root", { message: "E-mail ou senha incorretos." });
         return;
       }
 
       navigate("/");
     } catch (error: any) {
-      setError("root", { message: "Ocorreu um erro na tentativa de login." });
+      form.setError("root", {
+        message: "Ocorreu um erro na tentativa de login.",
+      });
     }
   };
 
   return (
     <section>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-100 dark:border-gray-200">
-          <div className="justify-self-center p-12">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm pb-12">
-              <img
-                src="/soberj.png"
-                alt="Soberj Logo"
-                width={900}
-                height={900}
-              />
-            </div>
+        <Card className="w-[350px]">
+          <CardHeader>
+            <img src="/soberj.png" alt="Soberj Logo" width={900} height={900} />
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                action="#"
+                method="POST"
+              >
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Email" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Senha</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="Senha"
+                              {...field}
+                              type="password"
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Button type="submit">Entrar</Button>
+                  </div>
 
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="space-y-4 md:space-y-6"
-              action="#"
-              method="POST"
-            >
-              <div>
-                <input
-                  {...register("email")}
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="Email"
-                  autoComplete="email"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 mb-2 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                {errors.email && (
-                  <p className="text-red-400 text-xs italic">
-                    *Email incorreto
-                  </p>
-                )}
-              </div>
-
-              <div>
-                <input
-                  {...register("password")}
-                  id="password"
-                  name="password"
-                  type="password"
-                  placeholder="Senha"
-                  autoComplete="current-password"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 mb-2 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                {errors.password && (
-                  <p className="text-red-400 text-xs italic">
-                    *Senha incorreta
-                  </p>
-                )}
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Entrar
-                </button>
-              </div>
-
-              <div>
-                {errors.root && (
-                  <p className="text-red-500 text-xs italic text-center">
-                    *Login ou senha incorretos
-                  </p>
-                )}
-              </div>
-            </form>
-          </div>
-        </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <FormMessage>
+                      {form?.formState?.errors?.root?.message}
+                    </FormMessage>
+                  </div>
+                </div>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
       </div>
     </section>
   );
