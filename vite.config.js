dbf00eb0bec12028977a476150b77b0981a6250d
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-04-08 13:50:03 -0300

    adiciona alias para importacao '@/components' ao vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import path from 'path';
+
+const pathConfig = path.resolve(path.resolve(), 'src')
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: pathConfig},
+    ],
+  },
 })
