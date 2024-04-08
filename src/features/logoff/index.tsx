f479e83736ccc60b5c7a636ce5b6f60576bf2e4e
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-04-08 13:50:57 -0300

    add limpeza de localStorage ao logoff

--- src/features/logoff/index.tsx
+++ src/features/logoff/index.tsx
@@ -2,5 +2,6 @@ import { Navigate, useLocation } from "react-router-dom";
 
 export default function Logoff() {
   const location = useLocation();
+  localStorage.clear();
   return <Navigate to="/login" state={{ from: location }} />;
 }
