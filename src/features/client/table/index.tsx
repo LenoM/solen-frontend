edf5c1e3f0ca1eb4e75d56001cc8c8bed5f112d2
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-04-13 08:18:28 -0300

    feature: tabela para listar clientes

---
+++ src/features/client/table/index.tsx
@@ -0,0 +1,49 @@
+import { Eye } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+
+type Client = {
+  id: string;
+  cpf: string;
+  name: string;
+  kinship: string;
+  isActive: boolean;
+};
+
+export const columns: ColumnDef<Client>[] = [
+  {
+    accessorKey: "id",
+    header: "Id",
+  },
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "cpf",
+    header: "CPF",
+  },
+  {
+    accessorKey: "isActive",
+    header: "Ativo?",
+    accessorFn: (status: Client) => (status.isActive === true ? "Sim" : "Não"),
+  },
+  {
+    accessorKey: "kinship",
+    header: "Titular?",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <Link to={`${row.id}`}>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <Eye className="h-4 w-4" />
+          </Button>
+        </Link>
+      );
+    },
+  },
+];
