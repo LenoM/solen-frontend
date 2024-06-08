5caec4a981367e530794d6dd5a1e8a786306c2fa
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-06-07 22:18:21 -0300

    add react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import { Toaster } from "@/components/ui/sonner";
 import { ErrorPage } from "@/components/error-page";
+import { queryClient } from "@/lib/react-query";
 import App from "./App.jsx";
 import "./assets/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<ErrorPage />}>
-      <App />
-      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+        <Toaster />
     </ErrorBoundary>
   </React.StrictMode>
 );
