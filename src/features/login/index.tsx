7715d66612a8a6edb8899aeb2ce4124fcd79336e
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-06-07 22:21:02 -0300

    simplifica useLogin

--- src/features/login/index.tsx
+++ src/features/login/index.tsx
@@ -29,7 +29,7 @@ const loginSchema = yup.object({
     .min(5, ErrorMessage.invalidPassword),
 });
 
-type LoginType = yup.InferType<typeof loginSchema>;
+export type LoginType = yup.InferType<typeof loginSchema>;
 
 export default function Login() {
   const login = useLogin();
--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,42 +1,14 @@
 import { useNavigate } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
+
 import { toast } from "sonner";
+import type { LoginType } from "@/features/login";
+import { setToken } from "@/utils/local-storage-utils";
 import { SERVER_ERROR_MESSAGE } from "@/utils/error.enum";
-import { LoginType } from "@/features/login";
 
 const url = import.meta.env.VITE_API_URL + "/auth/login";
 
 const useLogin = () => {
   const navigate = useNavigate();
-
-  const setToken = (email: string, token: string) => {
-    localStorage.setItem("isLoggedIn", "true");
-    localStorage.setItem("username", email);
-    localStorage.setItem("accessToken", token);
-  };
-
-  const isLoggedIn = (): boolean => {
-    return Boolean(localStorage.getItem("isLoggedIn")) === true;
-  };
-
-  const isExpired = (): boolean => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      const decoded = jwtDecode(token);
-
-      if (decoded && decoded.exp) {
-        const expired = decoded.exp * 1000 < Date.now();
-
-        if (!expired) {
-          return false;
-        }
-      }
-    }
-
-    localStorage.clear();
-
-    return true;
-  };
 
   const onLogin = async ({ email, password }: LoginType) => {
     const headers = { "Content-Type": "application/json" };
@@ -74,8 +46,6 @@ const useLogin = () => {
 
   return {
     onLogin,
-    isExpired,
-    isLoggedIn,
   };
 };
 
