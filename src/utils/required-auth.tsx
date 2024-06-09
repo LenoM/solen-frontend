f11f6d71d952abdcf208f900567c580b98acab18
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-06-08 22:39:16 -0300

    melhorias lint

--- src/utils/required-auth.tsx
+++ src/utils/required-auth.tsx
@@ -1,12 +1,9 @@
 import { Navigate, useLocation } from "react-router-dom";
-import useLogin from "@/hooks/useLogin";
 
 import Layout from "@/components/default-page";
 import { isExpired, isLoggedIn } from "@/utils/local-storage-utils";
 
 const RequireAuth = () => {
-  const login = useLogin();
-
   if (!isLoggedIn() || isExpired()) {
     const location = useLocation();
     return <Navigate to="/login" state={{ from: location }} />;
