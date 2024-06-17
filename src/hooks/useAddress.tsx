75d5e5319cfcc412b6ee12a2ac992a857bc5878c
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-06-17 09:55:28 -0300

    atualiza import type

--- src/hooks/useAddress.tsx
+++ src/hooks/useAddress.tsx
@@ -1,11 +1,11 @@
 import { toast } from "sonner";
 import { useState } from "react";
 
-import { AddressDataType } from "@/features/client/forms/address";
+import type { AddressDataType } from "@/features/client/forms/address";
+import type { ClientType } from "@/features/client/forms/personal";
 import { SERVER_ERROR_MESSAGE } from "@/utils/error.enum";
 import { getHeader } from "@/utils/headers-utils";
 import { queryClient } from "@/lib/react-query";
-import { ClientType } from "@/features/client/forms/personal";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
