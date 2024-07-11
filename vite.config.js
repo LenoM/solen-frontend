07b64cf90189eee6eda8cece452c7c8a56e46b46
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-07-11 16:51:40 -0300

    add otimizacao para producao e rollup-plugin-visualizer para acompanhamento do tamanho do bundle

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { compression } from "vite-plugin-compression2";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 import path from "path";
 
@@ -13,14 +14,58 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: (id) => {
-          if (id.includes("radix-ui")) {
-            return "@radix-ui";
+          if (id.includes("react-hook-form") || id.includes("@hookform")) {
+            return "@hook";
+          }
+
+          if (id.includes("tailwind-merge")) {
+            return "@merge";
+          }
+
+          if (id.includes("node_modules/react/")) {
+            return "@dom-1";
+          }
+
+          if (id.includes("node_modules/react-dom")) {
+            return "@dom-2";
+          }
+
+          if (id.includes("table-core")) {
+            return "@table";
+          }
+
+          if (id.includes("@tanstack")) {
+            return "@query";
+          }
+
+          if (id.includes("node_modules/yup")) {
+            return "@forms";
+          }
+
+          if (id.includes("sonner")) {
+            return "@sonner";
+          }
+
+          if (id.includes("node_modules/validator")) {
+            return "@validator";
+          }
+
+          if (id.includes("react-router")) {
+            return "@router";
+          }
+
+          if (id.includes("react-icons")) {
+            return "@icon";
+          }
+
+          if (id.includes("radix-ui/react-dialog")) {
+            return "@ui-dialog";
           }
         },
       },
     },
   },
-  plugins: [react(), compression()],
+  plugins: [react(), compression(), visualizer()],
   resolve: {
     alias: [{ find: "@", replacement: pathConfig }],
   },
