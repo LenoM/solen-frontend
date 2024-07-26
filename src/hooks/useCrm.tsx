d21b8ee573b69ba8c0039c6a706a9997e29a6b8f
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-07-26 19:04:00 -0300

    add create ao useCrm

--- src/hooks/useCrm.tsx
+++ src/hooks/useCrm.tsx
@@ -2,6 +2,10 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { toast } from "sonner";
 import useFetcher from "@/lib/request";
+import { queryClient } from "@/lib/react-query";
+import { CrmHistoryType } from "@/features/crm/crm-form";
+import type { ClientType } from "@/features/client/client-schema";
+import type { CollectHistoryType } from "@/features/crm/collection-form";
 
 export default function useCrm() {
   const fetcher = useFetcher();
@@ -30,8 +34,54 @@ export default function useCrm() {
     setLoading(false);
   };
 
+  const createCrm = async (data: CrmHistoryType) => {
+    setLoading(true);
+
+    const {
+      returnDate,
+      description,
+      statusId,
+      motiveId,
+      originId,
+      subMotiveId,
+      clientId,
+      type,
+    } = data;
+
+    const body: BodyInit = JSON.stringify({
+      returnDate,
+      description,
+      statusId: Number(statusId),
+      motiveId: Number(motiveId),
+      subMotiveId: Number(subMotiveId),
+      originId: Number(originId),
+      clientId,
+      type,
+    });
+
+    const response = await fetcher.post<CrmHistoryType>("crm", body);
+
+    if (response) {
+      queryClient.setQueryData(
+        ["getClientById", { clientId }],
+        (prev: ClientType) => {
+          if (prev && prev.crmHistory) {
+            return { ...prev, crmHistory: [...prev.crmHistory, response] };
+          }
+        }
+      );
+
+      toast.success("Histórico de atendimento salvo", {
+        description: `O atendimento foi salvo`,
+      });
+    }
+
+    setLoading(false);
+  };
+
   return {
     loading,
     getNextCall,
+    createCrm,
   };
 }
