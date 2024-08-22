f527bcbf30e1e9ecdabfd8aff1d106c226149d5c
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-08-22 14:52:36 -0300

    melhorias de build

--- vite.config.js
+++ vite.config.js
@@ -13,19 +13,36 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: (id) => {
-          if (
-            id.includes("components/ui/calendar") ||
-            id.includes("date-fns")
-          ) {
-            return "@calendar";
+          if (id.includes("node_modules/react-dom")) {
+            return "@dom-1";
           }
 
-          if (id.includes("@radix-ui")) {
-            return "@ui-base";
+          if (id.includes("node_modules/react-router-dom/")) {
+            return "@dom-2";
           }
 
-          if (id.includes("components/input") || id.includes("components/ui")) {
-            return "@ui";
+          if (id.includes("node_modules/validator")) {
+            return "@validator";
+          }
+
+          if (id.includes("@tanstack/query-core")) {
+            return "@query";
+          }
+
+          if (id.includes("@tanstack/table-core")) {
+            return "@table";
+          }
+
+          if (id.includes("src/lib")) {
+            return "@lib";
+          }
+
+          if (id.includes("react-icons") || id.includes("lucide-react")) {
+            return "@icon";
+          }
+
+          if (id.includes("date-fns")) {
+            return "@date";
           }
 
           if (
@@ -34,73 +51,6 @@ export default defineConfig({
             id.includes("node_modules/yup")
           ) {
             return "@form";
-          }
-
-          if (id.includes("node_modules/react/")) {
-            return "@dom-1";
-          }
-
-          if (id.includes("node_modules/react-dom")) {
-            return "@dom-2";
-          }
-
-          if (id.includes("@tanstack")) {
-            return "@query";
-          }
-
-          if (id.includes("src/utils")) {
-            return "@util";
-          }
-
-          if (id.includes("node_modules/validator")) {
-            return "@validator";
-          }
-
-          if (id.includes("react-icons") || id.includes("lucide-react")) {
-            return "@icon";
-          }
-
-          if (id.includes("features/dashboard")) {
-            return "@dash";
-          }
-
-          if (id.includes("features/custom-report")) {
-            return "@report";
-          }
-
-          if (id.includes("features/user")) {
-            return "@user";
-          }
-
-          if (id.includes("features/login") || id.includes("features/logoff")) {
-            return "@login";
-          }
-
-          if (id.includes("features/crm")) {
-            return "@crm";
-          }
-
-          if (id.includes("features/product")) {
-            return "@prod";
-          }
-
-          if (
-            id.includes("features/invoice") ||
-            id.includes("features/batch-generator")
-          ) {
-            return "@inv";
-          }
-
-          if (id.includes("features/client/details")) {
-            return "@cli-det";
-          }
-
-          if (id.includes("features/client/table")) {
-            return "@cli-list";
-          }
-
-          if (id.includes("features/client/add")) {
-            return "@cli-add";
           }
         },
       },
