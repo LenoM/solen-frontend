baf0a8fe17fdd8a87e16ec2704c46881ab002390
Leno Mota <lenomota@Lenos-MacBook-Pro.local>
2024-08-27 14:53:27 -0300

    add component dropzone controlled

--- src/components/dropzone/index.tsx
+++ src/components/dropzone/index.tsx
@@ -1,7 +1,44 @@
+import React, { ChangeEvent, useRef } from "react";
+import { UseFormReturn } from "react-hook-form";
+import { FileCheck2Icon } from "lucide-react";
+import { mixed } from "yup";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+
+import { ErrorMessage } from "@/utils/error.enum";
 import { cn } from "@/lib/utils";
-import React, { ChangeEvent, useRef } from "react";
+
+const ACEPTED_DOC_TYPE = ["jpg", "png", "pdf"];
+
+const MAX_FILE_SIZE = 1024 * 1024 * 2; // 2MB
+
+const isValidFileType = (fileName: string) => {
+  const ext = fileName.split(".").pop() ?? "jpg";
+  const isValid = fileName && ACEPTED_DOC_TYPE.includes(ext);
+  return !!isValid;
+};
+
+const isValidFileSize = (value: File | null) => {
+  const isValid = value && value?.size <= MAX_FILE_SIZE;
+  return !!isValid;
+};
+
+export const fileBaseSchema = mixed<any>()
+  .required(ErrorMessage.invalidFile)
+  .test("is-valid-type", ErrorMessage.invalidFileFormat, (value) =>
+    isValidFileType(value?.name?.toLowerCase())
+  )
+  .test("is-valid-size", ErrorMessage.invalidFileLength, (value) =>
+    isValidFileSize(value)
+  );
 
 interface DropzoneProps
   extends Omit<
@@ -12,6 +49,75 @@ interface DropzoneProps
   className?: string;
   dropMessage: string;
   handleOnDrop: (acceptedFiles: FileList | null) => void;
+}
+
+type DropzoneControolledProps = {
+  form: UseFormReturn<any>;
+  name: string;
+};
+
+export default function DropzoneControlled(props: DropzoneControolledProps) {
+  function handleOnDrop(fileList: FileList | null) {
+    console.log("acceptedFiles", fileList);
+    if (fileList && fileList.length > 0) {
+      const file = fileList[0];
+
+      if (!isValidFileSize(file)) {
+        props.form.setValue(`${props.name}`, null);
+        props.form.setError(`${props.name}`, {
+          message: ErrorMessage.invalidFileLength,
+          type: "typeError",
+        });
+      } else if (!isValidFileType(file.name)) {
+        props.form.setValue(`${props.name}`, null);
+        props.form.setError(`${props.name}`, {
+          message: ErrorMessage.invalidFileFormat,
+          type: "typeError",
+        });
+      } else {
+        props.form.setValue(`${props.name}`, file);
+        props.form.clearErrors(`${props.name}`);
+      }
+    } else {
+      props.form.setValue(`${props.name}`, null);
+      props.form.setError(`${props.name}`, {
+        message: ErrorMessage.invalidFile,
+        type: "typeError",
+      });
+    }
+  }
+
+  return (
+    <>
+      <FormField
+        control={props.form.control}
+        name={props.name}
+        render={({ field }) => {
+          return (
+            <FormItem className="w-full">
+              <FormControl>
+                <Dropzone
+                  {...field}
+                  dropMessage="Click ou arraste os arquivos"
+                  handleOnDrop={handleOnDrop}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          );
+        }}
+      />
+
+      {props.form.watch(`${props.name}`) && (
+        <div className="flex items-center justify-center gap-3 p-4 relative">
+          <FileCheck2Icon className="h-4 w-4" />
+          <p className="text-sm font-medium">
+            {props.form.watch(`${props.name}`)?.name}
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
 
 const Dropzone = React.forwardRef<HTMLDivElement, DropzoneProps>(
@@ -76,5 +182,3 @@ const Dropzone = React.forwardRef<HTMLDivElement, DropzoneProps>(
     );
   }
 );
-
-export default Dropzone;
